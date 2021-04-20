b4c9f4a44f3ff0df1d3e1e9bcc9356c6d1adeaaf
Gabriel Buchdahl <gabriel@wanderlog.com>
2021-04-19 21:00:13 -0400

    width?

--- src/pages/alumni/index.js
+++ src/pages/alumni/index.js
@@ -8,7 +8,7 @@ const AlumniPage = () => {
             <h1 className='Hero__header text-center mt-4 mb-5'>Alumni Database</h1>
             <div className='row justify-content-center' style={{ marginTop: 30, height: '75vh' }}>
                 <div className='col-11 col-md-10'>
-                    <iframe title="alumni-spreadsheet" className='w-100 h-100' src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSWCnMaN_mn0pfoygvHgdc7yypHKHmfhx8g7psS-fZmBrVjGIefSjuTFshbzfKEgM4f4MwiIYZWApzk/pubhtml?widget=true&amp;headers=false"></iframe>
+                    <iframe style={{ width: '100%' }} title="alumni-spreadsheet" className='w-100 h-100' src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSWCnMaN_mn0pfoygvHgdc7yypHKHmfhx8g7psS-fZmBrVjGIefSjuTFshbzfKEgM4f4MwiIYZWApzk/pubhtml?widget=true&amp;headers=false"></iframe>
                 </div>
             </div>
         </Layout>
